b5611340c9c8ad6ce3dab570a772c1bb6d07acef
Patrushev Nikita <nick@NickMacBook.local>
2024-09-26 14:22:33 +0700

    Add label with full name on main screen

---
+++ homework_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homework_1
+//
+//  Created by Patrushev Nikita on 26.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
